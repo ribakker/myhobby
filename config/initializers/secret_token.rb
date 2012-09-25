cf0b8bcdd1e56d7b185dfa6594c34d9cf48cb3be
ribakker <richard@ribakker.nl>
2012-09-25 19:16:21 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myhobby::Application.config.secret_token = '7e9caba9b9e718211955875fe7372128c185a7416717508937e473ec65e64de4cbba6ede5c4836b84d5ca266e618c8dc3e9d983d8a7620dd3aaeb31133f95883'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Myhobby::Application.config.session_store :cookie_store, key: '_myhobby_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Myhobby::Application.config.session_store :active_record_store
